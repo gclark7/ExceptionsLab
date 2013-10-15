db5ded3c67bcc0fd7bdf062faf859c87c96eac15
gcDataTechnology <gcDataTechnology@gcDataTechnolog>
2013-10-14 22:33:01 -0500

    Exceptions Lab...Think outside the parameters...then account for handling it. Lab0 done

--- src/lab0/Employee.java
+++ src/lab0/Employee.java
@@ -20,6 +20,16 @@ import java.util.Date;
  */
 public class Employee {
     public static final int MAX_VACATION_DAYS = 28;
+    public static final int MIN_VACATION_DAYS=0;
+    private final String WRONG_NUM="Number is either higher or lower than necessary.";
+    private final String WRONG_NAME="Only alpha characters; no number or symbols...sorry Artist FKA Prince";
+    private final String NULL_VAL="Null or empty string";
+    private final String MAX_CHARS="Value has exceeded the maximum of 50 characters";
+    private final int MAX_ALPHA=50;
+    private final int MAX_SSN=9;
+    private final String WRONG_SSN="Only 9 numbers please";
+    private final String MAX_DIGITS="Too many numbers to be a SSN";
+    
     private String firstName;
     private String lastName;
     private String ssn;
@@ -34,52 +44,127 @@ public class Employee {
         daysVacation = 0;
     }
 
+    /**
+     * version 2 constructor utilizes the setter methods to validate values
+     * @param firstName
+     * @param lastName
+     * @param ssn
+     * @param hireDate
+     * @param daysVacation 
+     */
+    public Employee(String firstName, String lastName, String ssn, Date hireDate, int daysVacation) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setSsn(ssn);
+        this.setHireDate(hireDate);
+        this.setDaysVacation(daysVacation);
+    }
+    /*original constructor
     public Employee(String firstName, String lastName, String ssn, Date hireDate, int daysVacation) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.ssn = ssn;
         this.hireDate = hireDate;
         this.daysVacation = daysVacation;
-    }
+    }*/
     
     public int getDaysVacation() {
         return daysVacation;
     }
 
+    /**
+     * no one in this company has more than 28 days of vacation
+     * also checks to be sure number is not negative
+     * @param daysVacation 
+     */
     public void setDaysVacation(int daysVacation) {
-        this.daysVacation = daysVacation;
+        if(daysVacation>MAX_VACATION_DAYS){
+            throw new IllegalArgumentException(WRONG_NUM);
+        }else if(daysVacation<MIN_VACATION_DAYS){
+            throw new IllegalArgumentException(WRONG_NUM);
+        }else{this.daysVacation = daysVacation;}
     }
 
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * checks for null, 0-length, max characters, alpha characters
+     * 
+     * @param firstName 
+     */
     public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        if(firstName==null||firstName.length()==0){
+            throw new IllegalArgumentException(NULL_VAL);
+        }else if(firstName.length()>MAX_ALPHA){
+             throw new IllegalArgumentException(MAX_CHARS);
+        }else{
+            String pattern = "^[a-zA-Z]*$";
+            if(!firstName.matches(pattern)){
+                    throw new IllegalArgumentException(WRONG_NAME);
+            }else{ this.firstName = firstName;}
+        }
+       
     }
 
     public Date getHireDate() {
         return hireDate;
     }
 
+    /**
+     * checks for null and legitimate date
+     * @param hireDate 
+     */
     public void setHireDate(Date hireDate) {
+        if(hireDate==null){
+            throw new IllegalArgumentException(NULL_VAL);
+        }else{
         this.hireDate = hireDate;
+        }
     }
 
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Checks for null, 0-length, max length, min length and alpha characters
+     * @param lastName 
+     */
     public void setLastName(String lastName) {
-        this.lastName = lastName;
+         if(lastName==null||lastName.length()==0){
+            throw new IllegalArgumentException(NULL_VAL);
+        }else if(lastName.length()>MAX_ALPHA){
+             throw new IllegalArgumentException(MAX_CHARS);
+        }else{
+            String pattern = "^[a-zA-Z]*$";
+            if(!lastName.matches(pattern)){
+                    throw new IllegalArgumentException(WRONG_NAME);
+            }else{ this.lastName = lastName;}
+        }
     }
 
     public final String getSsn() {
         return ssn;
     }
 
+    /**
+     * Checks for null, 0-length, digits and max digits
+     * @param ssn 
+     */
     public void setSsn(String ssn) {
-        this.ssn = ssn;
+        
+        if(ssn==null||ssn.length()==0){
+            throw new IllegalArgumentException(NULL_VAL);
+        }else if(ssn.length()>MAX_SSN){
+             throw new IllegalArgumentException(MAX_DIGITS);
+        }else{
+            String pattern = "^[0-9]*$";
+            if(!ssn.matches(pattern)){
+                    throw new IllegalArgumentException(WRONG_SSN);
+            }else{ this.ssn = ssn;}
+        }
     }
     
     
