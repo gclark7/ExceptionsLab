935858afebccad933816af1ce14f59dc6d94cda1
gcDataTechnology <gcDataTechnology@gcDataTechnolog>
2013-10-14 23:50:40 -0500

    Exceptions Lab...Think outside the parameters...then account for handling it. Lab0 done Lab1 done Lab2 done

--- src/lab2/InputOutputGui.java
+++ src/lab2/InputOutputGui.java
@@ -9,17 +9,41 @@ import javax.swing.JOptionPane;
  * @version 1.00
  */
 public class InputOutputGui {
-    private NameService nameService;
+     private NameService nameService;
+     private final String REQUEST_FULL_NAME="Enter full name\n FirstName then LastName\n ***Please place a space between names***"+
+            "\nOnly Alpha characters";
+     private final String PRESENT_LAST_NAME="Your last name is: ";
 
     public InputOutputGui() {
         nameService = new NameService();
     }
 
+    /**
+     * The GUI is now responsible for output of illegal argument exceptions
+     * a do while loop ensures the input is verified and proper prior to extraction
+     * a try catch handles the last name extraction and the exceptions produced
+     */
     public void startConversation() {
         
-        String fullName = JOptionPane.showInputDialog("Enter full name:");
-        String lastName = nameService.extractLastName(fullName);
-        String msg = "Your last name is: " + lastName;
+        String fullName = JOptionPane.showInputDialog(REQUEST_FULL_NAME);
+        String lastName="";
+        String msg=""; 
+        boolean rightName=false;
+        
+        do{
+        try{
+            lastName= nameService.extractLastName(fullName);
+            rightName=true;
+        }catch(IllegalArgumentException e){
+            JOptionPane.showMessageDialog(null, e);
+            rightName=false;
+        }
+            if(!rightName){
+                fullName = JOptionPane.showInputDialog(REQUEST_FULL_NAME);
+            }
+        }while(!rightName);
+        
+        msg= PRESENT_LAST_NAME + lastName;
         JOptionPane.showMessageDialog(null, msg);
         
     }
