110de7a6feeb4cfe87d99d214e0707f73f5a7ae5
gcDataTechnology <gcDataTechnology@gcDataTechnolog>
2013-10-14 23:27:20 -0500

    Exceptions Lab...Think outside the parameters...then account for handling it. Lab0 done Lab1 done

--- src/lab1/Challenge1.java
+++ src/lab1/Challenge1.java
@@ -17,19 +17,69 @@ import javax.swing.JOptionPane;
  */
 public class Challenge1 {
     private static final int LAST_NAME_IDX = 1;
+    private static final String REQUEST_FULL_NAME="Enter full name\n FirstName then LastName\n ***Please place a space between names***"+
+            "\nOnly Alpha characters";
+    private static final String PRESENT_LAST_NAME="Your last name is: ";
+    private static final String WRONG_NAME="Needs a space with Alpha characters\n";
+    private static final String NULL_VAL="Null or empty string\n";
 
     public static void main(String[] args) {
         Challenge1 app = new Challenge1();
         
-        String fullName = JOptionPane.showInputDialog("Enter full name:");
+        String fullName = JOptionPane.showInputDialog(REQUEST_FULL_NAME);
         String lastName = app.extractLastName(fullName);
-        String msg = "Your last name is: " + lastName;
+        String msg =  PRESENT_LAST_NAME + lastName;
         JOptionPane.showMessageDialog(null, msg);
     }
     
+    /**
+     * finds last name starting at the last character and working forward to a space.
+     * If no space is found a prompt requests the proper format until it is correctly entered
+     * 
+     * @param fullName
+     * @return 
+     */
     public String extractLastName(String fullName) {
-        String[] nameParts = fullName.split(" ");
-        return nameParts[LAST_NAME_IDX];
+        String lastName="";
+        String msg="";
+        //String temp="";
+        //String pattern = "[a-zA-Z]"+ " " + "[a-zA-Z]";
+        boolean nameRight=false;
+        int j=0;
+        
+        do{
+            if(fullName==null||fullName.length()==0){
+                nameRight=false;
+               msg=NULL_VAL;
+            }else{
+                for(int i=fullName.length()-1;i>=0;i--){
+                    if(fullName.charAt(i)==32&&nameRight==false){
+                        nameRight=true;
+                        msg="";
+                        //extract last name
+                        for(j=i;j<fullName.length();j++){
+                            lastName+=fullName.toCharArray()[j];
+                        }
+                    }
+                 }
+            }
+        
+            if(!nameRight){
+                fullName = JOptionPane.showInputDialog(msg+REQUEST_FULL_NAME);
+            }
+        }while(!nameRight);
+        
+        
+        return lastName;
+        //String[] nameParts = fullName.split(" ");
+        //return nameParts[LAST_NAME_IDX];
+        //tried
+        /*
+         * else if(!fullName.matches(pattern)){
+                    nameRight=false;
+                    System.out.println("not matching pattern");
+         */
     }
 
+    
 }
